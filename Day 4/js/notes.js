15d5dcf4cfcd838f57a03627ab62f730451b7d9e
Rick Osborne <rosborne@fullsail.com>
2013-03-11 12:51:57 -0400

    Debugging, Part 2

--- Day 4/js/notes.js
+++ Day 4/js/notes.js
@@ -3,8 +3,16 @@ console.log("These are my notes");
 var phasers = parseInt(prompt("How many phasers?"));
 var setting = "stun";
 
-if ((phasers > 3) || (setting == "kill")) {
+console.log("phasers:" + phasers);
+console.log("setting:" + setting);
+
+var atLeast3Phasers = (phasers > 3);
+var setToKill = (setting == "kill");
+
+if (atLeast3Phasers || setToKill) {
     console.log("It's dead, Jim.");
+} else if (setting == "stun") {
+    console.log("That won't be enough");
 } else {
     console.log("Run faster than the red shirt!");
 }
