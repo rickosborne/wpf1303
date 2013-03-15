20d8f5490ef4fe9e8a5e9bf493ed205cf523eeb7
Rick Osborne <rosborne@fullsail.com>
2013-03-15 10:26:18 -0400

    Project Euler #3

--- Day 5/carlos.js
+++ Day 5/carlos.js
@@ -1,7 +1,44 @@
-/**
- * Created with JetBrains WebStorm.
- * User: rosborne
- * Date: 3/15/13
- * Time: 09:19 AM
- * To change this template use File | Settings | File Templates.
- */
+//i=0 - start from 0
+//i < 1000 - check number from 0 - 1000
+//i++ - number + 1
+
+//Problem 1
+var total = 0 ;
+
+for (var i = 0; i < 1000; i++) {
+    if ((i % 3 == 0) || (i % 5 == 0)){
+        total = total + i;
+    }
+}
+console.log(total);
+
+//Problem 2
+var max = 4000000;
+var a = 1;
+var b = 2;
+var c = 0; // accumulator
+var evenFibs = [];
+var total2 = 0;
+
+while (a < max){
+    if (a % 2 == 0){
+        total2 += a;
+        evenFibs.push(a);
+    }
+    c = a + b;
+    a = b;
+    b = c;
+}
+console.log(total2);
+console.log(evenFibs);
+
+a = b + c;
+if ((a % 2 == 0) && (a < max)) {
+    total2 += a;
+}
+b = a + c;
+if ((b % 2 == 0) && (b < max)) {
+    total2 += b;
+}
+console.log(total2);
+
--- Day 6/euler.js
+++ Day 6/euler.js
@@ -1,7 +1,41 @@
-/**
- * Created with JetBrains WebStorm.
- * User: rosborne
- * Date: 3/15/13
- * Time: 09:35 AM
- * To change this template use File | Settings | File Templates.
- */
+console.log("Running euler.js");
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+// Find the factors
+var factors = []; // declaring the array
+var bigNumber = 600851475143;
+console.log(bigNumber);
+var currentNumber = 1;
+while (currentNumber < Math.sqrt(bigNumber)) {
+    // Factors are numbers that go into a larger number evenly
+    if ((bigNumber % currentNumber) == 0) {
+        console.log("Factor: " + currentNumber);
+        factors.push(currentNumber);
+    }
+    currentNumber += 1;
+}
+console.log(factors);
+
+// Figure out which factors are prime
+// Prime numbers are only divisible by 1 and that number
+var primeFactors = [];
+for (var arrayIndex = 0 ; arrayIndex < factors.length ; arrayIndex++ ) {
+    // For each item in my array ...
+    var factor = factors[arrayIndex];
+    var isPrime = true;
+    for (var divisor = 2 ; (divisor < Math.sqrt(factor)) && isPrime; divisor++ ) {
+        if ((factor % divisor) == 0) {
+            isPrime = false;
+        } // if is a factor
+    } // for divisor
+    if (isPrime) {
+        primeFactors.push(factor);
+    } // if isPrime
+} // for arrayIndex
+console.log(primeFactors);
+
+// Find the largest prime
+var biggestPrime = primeFactors.pop();
+console.log("The largest prime factor of " + bigNumber + " is " + biggestPrime);
